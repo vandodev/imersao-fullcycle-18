df05bcc9b1d1359efb4bbdaee5838f69816e4bce
vandodev <vandoaparecido@hotmail.com>
2024-06-17 08:02:02 -0300

    :ticket: :key: :unlock: Comando nest g resource

--- partners-api/src/app.controller.ts
+++ partners-api/src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// http://localhost:3000/prefixo/test
+// @Controller('prefixo')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/test')
   getHello(): string {
     return this.appService.getHello();
   }

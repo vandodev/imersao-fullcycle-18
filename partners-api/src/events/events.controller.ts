bac1e9ec3158d85728dc2bd743211b0f2eda5fef
vandodev <vandoaparecido@hotmail.com>
2024-06-18 09:23:35 -0300

    :ticket: :key: :unlock: Fazendo requisições com a exteção rest client do vscode

--- partners-api/src/events/events.controller.ts
+++ partners-api/src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,HttpCode } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
@@ -27,6 +27,7 @@ export class EventsController {
     return this.eventsService.update(id, updateEventDto);
   }
 
+  @HttpCode(204)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.eventsService.remove(id);

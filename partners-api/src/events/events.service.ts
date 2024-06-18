4d8ce009df362d798e48c9d8d78550d8a86499f8
vandodev <vandoaparecido@hotmail.com>
2024-06-18 08:03:58 -0300

    :ticket: :key: :unlock: Integrando prisma com nest

--- partners-api/src/events/events.service.ts
+++ partners-api/src/events/events.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EventsService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createEventDto: CreateEventDto) {
     return 'This action adds a new event';
   }
